3c0bf896ce5848c032b3928e4b218e5d4613f2d1
NeelCheo <73446815+NeelCheo@users.noreply.github.com>
2023-05-18 16:39:18 -0400

    setup the sections for the portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import About from './components/About/About';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+import Resume from './components/Resume/Resume';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Navigation from './components/Navigation/Navigation';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header>
+          <Navigation />
+        </Header>
+
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <About />
+            </Route>
+
+            <Route path="/portfolio">
+              <Portfolio />
+            </Route>
+
+            <Route path="/contact">
+              <Contact />
+            </Route>
+
+            <Route path="/resume">
+              <Resume />
+            </Route>
+          </Switch>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css'; // Create this file to add styles
+
+function About() {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <img src="your-photo-url.jpg" alt="Your Name" />
+      <p>
+        Short bio about you...
+      </p>
+    </section>
+  );
+}
+
+export default About;
